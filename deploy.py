c927b012352552e9a67c5dfdcd6a0283bc0d9f29
Luke Chatton <chattonluke@gmail.com>
2016-08-17 20:29:35 -0700

    better file names

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ from subprocess import call
 
 class GitAutoDeploy(BaseHTTPRequestHandler):
 
-    CONFIG_FILEPATH = './GitAutoDeploy.conf.json'
+    CONFIG_FILEPATH = './config.json'
     config = None
     quiet = False
     daemon = False
